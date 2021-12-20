a48c35f78880c58f390c322ee0b2a243f3166568
cheblankenshipUTD <che.blankenship@utdallas.edu>
2021-12-20 01:05:00 -0600

    encapsulated the modules in class LinkedList

--- c++code/datastructure/list/linked-list/main.cpp
+++ c++code/datastructure/list/linked-list/main.cpp
@@ -19,219 +19,259 @@ struct Node
 };
 
 
-int Count(struct Node* p)
+class LinkedList
 {
-    int count = 0;
 
-    // case 1: Head is initialized but no data/ no linked list
-    if (p != NULL && (p->data == INT_MIN && p->next == NULL) )
-        return count;
-    
-    // case 2: Link list exist and hits the tail (NULL value)
-    while (p != NULL)
+private:
+    struct Node* head = new Node();
+
+public:
+    LinkedList(){};
+    ~LinkedList(){};
+
+
+    struct Node* getHead()
     {
-        count++;
-        p = p->next;
-    }
-    
-    return count;
-}
-
-
-// Display linked list
-void Display(struct Node* p)
-{
-    while (p != NULL)
-    {
-        printf("%d ", p->data);
-        p = p->next;
-    }
-    printf("\n");
-}
-
-void DisplayRec(struct Node* p)
-{
-    // Case 1: if head is initialized but not a linked list
-    if (p != NULL && p->data == INT_MIN)
-        return;
-    
-    // Case 2: check if node reached the tail
-    if (p == NULL)
-        return;
-    
-    printf("%d ", p->data);
-    return DisplayRec(p->next);
-}
-
-// Create linked list from array
-void Create(int A[], int size, struct Node* head)
-{
-    struct Node* temp;
-    struct Node* last;
-
-    head->data = A[0];
-    head->next = NULL;
-    last = head;
-
-    // create link list
-    for (int i = 1; i < size; i++)
-    {
-        temp = new Node();
-        temp->data = A[i];
-        temp->next = NULL;
-        last->next = temp;
-        last = temp;
+        return head;
     }
 
-}
-
-int Sum(struct Node* p)
-{
-    int sum = 0;
-
-    // 1: Head declared but linked list not created
-    if (p != NULL && (p->data == INT_MIN && p->next == NULL))
-        return 0;
-    
-
-    while (p != NULL)
+    int Count()
     {
-        sum = sum + p->data;
-        p = p->next;
-    }
-    
-    return sum;
-}
+        int count = 0;
+        struct Node* hPtr = head;
 
-
-int MaxNode(struct Node* p)
-{
-    int max = INT_MIN;
-    if (Count(p) != 0)
-    {
-        while (p != NULL)
+        // case 1: Head is initialized but no data/ no linked list
+        if (hPtr != NULL && (hPtr->data == INT_MIN && hPtr->next == NULL) )
+            return count;
+        
+        // case 2: Link list exist and hits the tail (NULL value)
+        while (hPtr != NULL)
         {
-            max = std::max(max, p->data);
-            p = p->next;
+            count++;
+            hPtr = hPtr->next;
         }
-    }
-    return max;
-}
-
-bool Search(struct Node* p, int target)
-{
-    while (p != NULL)
-    {
-        if (p->data == target)
-            return true;
-        p = p->next;
-    }
-    return false;
-}
-
-
-// p: head pointer
-// data: new node data
-// position: insert to tail if position is not specified
-void Insert(struct Node** p, int data, int position)
-{
-    struct Node* tail = NULL;
-    struct Node* newNode = new Node(data);
-
-    // Case 1: insert in head
-    if (position == 0 || position == INT_MIN)
-    {
-        newNode->next = *p;
-        *p = newNode;
-        return;
+        
+        return count;
     }
 
-    // Case 2: insert after nth node
-    tail = *p;
-    for (int i = 1; i < position; i++)
+
+    // Display linked list
+    void Display()
     {
-        if (tail->next == NULL)
+        struct Node* hPtr = head;
+        while (hPtr != NULL)
         {
-            printf("Out of range. Inserting to tail.\n");
-            tail->next = newNode;
+            printf("%d ", hPtr->data);
+            hPtr = hPtr->next;
+        }
+        printf("\n");
+    }
+
+
+    void DisplayRec(struct Node* p)
+    {
+        // Case 1: if head is initialized but not a linked list
+        if (p != NULL && p->data == INT_MIN)
+            return;
+        
+        // Case 2: check if node reached the tail
+        if (p == NULL)
+            return;
+        
+        printf("%d ", p->data);
+        return DisplayRec(p->next);
+    }
+
+    // Create linked list from array
+    void Create(int A[], int size)
+    {
+        struct Node* hPtr = head;
+        struct Node* temp;
+        struct Node* last;
+
+        hPtr->data = A[0];
+        hPtr->next = NULL;
+        last = hPtr;
+
+        // create link list
+        for (int i = 1; i < size; i++)
+        {
+            temp = new Node();
+            temp->data = A[i];
+            temp->next = NULL;
+            last->next = temp;
+            last = temp;
+        }
+
+    }
+
+    int Sum()
+    {
+        struct Node* hPtr = head;
+        int sum = 0;
+
+        // 1: Head declared but linked list not created
+        if (hPtr != NULL && (hPtr->data == INT_MIN && hPtr->next == NULL))
+            return 0;
+        
+
+        while (hPtr != NULL)
+        {
+            sum = sum + hPtr->data;
+            hPtr = hPtr->next;
+        }
+        
+        return sum;
+    }
+
+
+    int MaxNode()
+    {
+        struct Node* hPtr = head;
+        int max = INT_MIN;
+        if (Count() != 0)
+        {
+            while (hPtr != NULL)
+            {
+                max = std::max(max, hPtr->data);
+                hPtr = hPtr->next;
+            }
+        }
+        return max;
+    }
+
+    bool Search(int target)
+    {
+        struct Node* hPtr = head; 
+        while (hPtr != NULL)
+        {
+            if (hPtr->data == target)
+                return true;
+            hPtr = hPtr->next;
+        }
+        return false;
+    }
+
+
+    // p: head pointer
+    // data: new node data
+    // position: insert to tail if position is not specified
+    void Insert(int data, int position)
+    {
+        struct Node** pp = &head;
+        struct Node* tail = NULL;
+        struct Node* newNode = new Node(data);
+
+        // Case 1: insert in head
+        if (position <= 0)
+        {
+            newNode->next = *pp;
+            *pp = newNode;
             return;
         }
-        tail = tail->next;
-    }
-    newNode->next = tail->next;
-    tail->next = newNode;
-}
 
-void Delete(struct Node** head, int target)
-{
-    // get the head address as a reference
-    struct Node* current = *head;
-    struct Node* tail = new Node();
-
-    // delete head node
-    if (current->data == target)
-    {
-        *head = current->next;
-        free(current);
-        return;
+        // Case 2: insert after nth node
+        tail = *pp;
+        for (int i = 1; i < position; i++)
+        {
+            if (tail->next == NULL)
+            {
+                printf("Out of range. Inserting to tail.\n");
+                tail->next = newNode;
+                return;
+            }
+            tail = tail->next;
+        }
+        newNode->next = tail->next;
+        tail->next = newNode;
     }
-    
-    // iterate up to the node we want to delete
-    while (current->next != NULL)
+
+    void Delete(int target)
     {
+        // get the head address as a reference
+        struct Node** pp = &head;
+        struct Node* current = *pp;
+        struct Node* tail = new Node();
+
+        // delete head node
         if (current->data == target)
         {
-            tail->next = current->next;
+            *pp = current->next;
             free(current);
             return;
         }
-        // move tail ptr to current ptr node
-        tail = current;
-        // move current ptr to next ptr node
-        current = current->next;
-    }
+        
+        // iterate up to the node we want to delete
+        while (current->next != NULL)
+        {
+            if (current->data == target)
+            {
+                tail->next = current->next;
+                free(current);
+                return;
+            }
+            // move tail ptr to current ptr node
+            tail = current;
+            // move current ptr to next ptr node
+            current = current->next;
+        }
 
-    printf("Target not found.\n");
-    
-}
+        printf("Target not found.\n");
+        
+    }
+};
+
+// LinkedList::LinkedList(/* args */)
+// {
+// }
+
+// LinkedList::~LinkedList()
+// {
+// }
+
+
+
+
 
 
 int main(int argc, char const *argv[])
 {
     int A[] = {3, 5, 4, 7, 99};
-    struct Node* head = new Node();
 
+    LinkedList* ll = new LinkedList();
+    
     // create
-    Create(A, sizeof(A)/sizeof(A[0]), head);
+    ll->Create(A, sizeof(A)/sizeof(A[0]));
 
     // display
-    DisplayRec(head);
+    ll->Display();
+    ll->DisplayRec(ll->getHead());
 
     // count number of nodes
-    printf("\n# of nodes: %d \n", Count(head));
+    printf("\n# of nodes: %d \n", ll->Count());
 
-    // sum of all node data
-    printf("\nSum of all nodes: %d \n", Sum(head));
+    // // sum of all node data
+    printf("\nSum of all nodes: %d \n", ll->Sum());
 
-    // find max node
-    printf("\nMax node: %d \n", MaxNode(head));
+    // // find max node
+    printf("\nMax node: %d \n", ll->MaxNode());
 
-    // search key
-    printf("\nDoes %d exist in the list?: %s \n", 99, Search(head, 99) ? "true" : "false");
+    // // search key
+    printf("\nDoes %d exist in the list?: %s \n", 99, ll->Search(99) ? "true" : "false");
 
     // insert 
-    Insert(&head, 100, 0);
-    Insert(&head, 54, 2);
-    Insert(&head, 32, 1000);
-    DisplayRec(head);
+    ll->Insert(100, 0);
+    ll->Insert(54, 2);
+    ll->Insert(32, 1000);
+    ll->DisplayRec(ll->getHead());
 
-    Delete(&head, 100);
+    ll->Delete(100);
     printf("\n");
-    DisplayRec(head);
+    ll->DisplayRec(ll->getHead());
 
-    Delete(&head, 4);
+    ll->Delete(4);
     printf("\n");
-    DisplayRec(head);
+    ll->DisplayRec(ll->getHead());
 
     return 0;
 }
